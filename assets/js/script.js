5e4693245b672093281a933c4e436b86b3d56e4c
Yash Agarwal <143737777+Yashagx@users.noreply.github.com>
2025-05-12 13:49:38 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["web designing", "backend development", "Freelancing", "Digital Creation", "Machine Whispering"],
+    strings: ["web designing", "backend development", "Freelancing", "Digital Creation", "Machine Whispering","Ethical Hacking"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
